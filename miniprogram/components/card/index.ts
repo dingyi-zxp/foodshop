2935461c9a1661f65b9e6d3919f456b61924e3d9
zhuangxingpeng <dy_zxp@163.com>
2022-12-20 02:35:26 +0800

    feat: haedr are layout of basic and right not have function also left alone slot

---
+++ miniprogram/components/card/index.ts
@@ -0,0 +1,66 @@
+import { more as more_icon } from "../../utils/icons";
+
+// card component
+const prefix_color = "color-background-";
+Component({
+	properties: {
+		// letf and right of margin
+		lr_margin: {
+			type: String,
+			value: "10px",
+		},
+		// up and down right of margin
+		ud_margin: {
+			type: String,
+			value: "0px",
+		},
+		color_header: {
+			type: String,
+			value: "gray",
+		},
+		color_body: {
+			type: String,
+			value: "red",
+		},
+		// logo
+		logo: {
+			type: String,
+			value: "https://ssl-pubpic.51yund.com/1326697398.jpg",
+		},
+		title: {
+			type: String,
+			value: "很长的文字eeeee",
+		},
+	},
+
+	attached: function () {
+		this.init_data();
+		console.log(more_icon);
+	},
+
+	data: {
+		colors: {
+			header: "gray",
+			body: "white",
+		},
+	},
+
+	methods: {
+		/**
+		 * init data
+		 * - color
+		 */
+		init_data: function () {
+			const data = this.data;
+			let colors = {
+				header: prefix_color + data.color_header,
+				body: prefix_color + data.color_body,
+			};
+
+			this.setData({
+				colors: colors,
+				more_icon: more_icon,
+			});
+		},
+	},
+});
---
+++ miniprogram/utils/icons.ts
@@ -0,0 +1,1 @@
+export const more = "https://ssl-pubpic.51yund.com/1327136796.png";
